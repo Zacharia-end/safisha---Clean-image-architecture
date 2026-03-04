0fd80217f0397fc75160a03860a6ccae327ed64c
Zacharia-end <zachariahbioto2@gmail.com>
2026-03-04 10:37:37 +0000

    Add PostgreSQL migrations to build

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "=====> Installing requirements..."
 pip install -r requirements.txt
 
-python ecommerce/manage.py collectstatic --no-input
-python ecommerce/manage.py migrate
+echo "=====> Changing to ecommerce directory..."
+cd ecommerce
+
+echo "=====> Collecting static files..."
+python manage.py collectstatic --no-input
+
+echo "=====> Running migrations..."
+python manage.py migrate --noinput
+
+echo "=====> Build complete!"
